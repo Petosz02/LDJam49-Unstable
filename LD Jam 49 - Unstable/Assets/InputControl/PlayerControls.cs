c155359348fd485a88cbcf42039f53d5f60f513f
Petosz02 <petorgeri@gmail.com>
2021-10-03 00:26:46 +0200

    progress

--- LD Jam 49 - Unstable/Assets/InputControl/PlayerControls.cs
+++ LD Jam 49 - Unstable/Assets/InputControl/PlayerControls.cs
@@ -22,7 +22,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""name"": ""Move"",
                     ""type"": ""Value"",
                     ""id"": ""28b571b6-c5e8-47ba-a9d6-5a563bf7df72"",
-                    ""expectedControlType"": """",
+                    ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """"
                 },
@@ -39,6 +39,14 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""type"": ""Button"",
                     ""id"": ""989dbf64-05e3-4c8c-8ae0-1c2d6c305d0f"",
                     ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Value"",
+                    ""id"": ""34dd1468-646b-4b73-9aae-04890ee59efa"",
+                    ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """"
                 }
@@ -61,7 +69,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Keyboard>/w"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
@@ -72,7 +80,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Keyboard>/s"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
@@ -83,7 +91,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
@@ -94,10 +102,21 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1ab4496d-8ec8-4bbd-abed-e8941baafd56"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 },
                 {
                     ""name"": """",
@@ -105,7 +124,18 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Mouse>/leftButton"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b072bd8e-735f-4bb6-a4bc-04afc90162ba"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
                     ""action"": ""Fire"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
@@ -116,21 +146,66 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""path"": ""<Mouse>/rightButton"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": ""Keyboard"",
                     ""action"": ""Use"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""332b8134-c396-4f3c-b031-a4f47978a4eb"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Use"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cf9395da-b136-47fb-bf9d-1d0217f87a6e"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d2add7f6-ed78-4094-b4ca-6bd9a29ecc24"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Aim"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
             ]
         }
     ],
-    ""controlSchemes"": []
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": []
+        },
+        {
+            ""name"": ""Controller"",
+            ""bindingGroup"": ""Controller"",
+            ""devices"": []
+        }
+    ]
 }");
         // Movement
         m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
         m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
         m_Movement_Fire = m_Movement.FindAction("Fire", throwIfNotFound: true);
         m_Movement_Use = m_Movement.FindAction("Use", throwIfNotFound: true);
+        m_Movement_Aim = m_Movement.FindAction("Aim", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -183,6 +258,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
     private readonly InputAction m_Movement_Move;
     private readonly InputAction m_Movement_Fire;
     private readonly InputAction m_Movement_Use;
+    private readonly InputAction m_Movement_Aim;
     public struct MovementActions
     {
         private @PlayerControls m_Wrapper;
@@ -190,6 +266,7 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         public InputAction @Move => m_Wrapper.m_Movement_Move;
         public InputAction @Fire => m_Wrapper.m_Movement_Fire;
         public InputAction @Use => m_Wrapper.m_Movement_Use;
+        public InputAction @Aim => m_Wrapper.m_Movement_Aim;
         public InputActionMap Get() { return m_Wrapper.m_Movement; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -208,6 +285,9 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @Use.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnUse;
                 @Use.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnUse;
                 @Use.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnUse;
+                @Aim.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnAim;
+                @Aim.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnAim;
+                @Aim.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnAim;
             }
             m_Wrapper.m_MovementActionsCallbackInterface = instance;
             if (instance != null)
@@ -221,14 +301,36 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                 @Use.started += instance.OnUse;
                 @Use.performed += instance.OnUse;
                 @Use.canceled += instance.OnUse;
+                @Aim.started += instance.OnAim;
+                @Aim.performed += instance.OnAim;
+                @Aim.canceled += instance.OnAim;
             }
         }
     }
     public MovementActions @Movement => new MovementActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    private int m_ControllerSchemeIndex = -1;
+    public InputControlScheme ControllerScheme
+    {
+        get
+        {
+            if (m_ControllerSchemeIndex == -1) m_ControllerSchemeIndex = asset.FindControlSchemeIndex("Controller");
+            return asset.controlSchemes[m_ControllerSchemeIndex];
+        }
+    }
     public interface IMovementActions
     {
         void OnMove(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
         void OnUse(InputAction.CallbackContext context);
+        void OnAim(InputAction.CallbackContext context);
     }
 }
--- LD Jam 49 - Unstable/Assets/Scripts/PlayerHandler.cs
+++ LD Jam 49 - Unstable/Assets/Scripts/PlayerHandler.cs
@@ -4,18 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerHandler : MonoBehaviour {
+
+    public static PlayerHandler Instance;
+
     private Rigidbody2D rigidBody;
     private Vector2 moveDir;
+    private Vector2 mousePos;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private Camera main;
+    [SerializeField] private Transform gun;
 
     PlayerControls inputActions;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
         inputActions = new PlayerControls();
 
         inputActions.Movement.Enable();
@@ -23,6 +33,8 @@ public class PlayerMovement : MonoBehaviour {
         inputActions.Movement.Fire.performed += Fire;
         inputActions.Movement.Use.performed += Use;
         //inputActions.Movement.Move.performed += ctx => moveDir = ctx.ReadValue<Vector2>();
+
+        main = Camera.main;
     }
 
     public void Use(InputAction.CallbackContext ctx)
@@ -39,16 +51,31 @@ public class PlayerMovement : MonoBehaviour {
     void Update()
     {
         moveDir = inputActions.Movement.Move.ReadValue<Vector2>();
+        mousePos = inputActions.Movement.Aim.ReadValue<Vector2>();
+        Vector2 pos = main.WorldToScreenPoint(transform.position);
+        //Debug.Log(mousePos);
+
+        Vector2 offset = mousePos - pos;
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        Debug.Log(offset);
+
+        if(mousePos.x < pos.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gun.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gun.localScale = Vector3.one;
+        }
+
+        gun.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     public void FixedUpdate()
     {
         rigidBody.velocity = moveDir * speed;
-    }
-
-    public void InputHandling(InputAction.CallbackContext ctx)
-    {
-        //
-        //Debug.Log(moveDir);
     }
 }
